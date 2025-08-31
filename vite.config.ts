03fbf35afdc0eea692a3b047a92ffa9ec7ddc2b4
Nassim <nassim.amokrane@hotmail.com>
2025-08-31 20:18:23 +0200

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   resolve: { alias: { "@": "/src" } },
-  base: '/${REPO_NAME}/',
-})
+  base: "/",
+});
